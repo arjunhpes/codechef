5280efae6a06a3551142199b3acf57b7fd2db4e7
Ritesh <riteshsingla@gmail.com>
2020-07-26 20:31:24 +0530

    Keeping just the class and usage. This would make it easy to maintain consistency.

--- templates/budget_cost_map.cc
+++ templates/budget_cost_map.cc
@@ -23,83 +23,6 @@
 //  IN THE SOFTWARE.
 //
 
-#include <algorithm>
-#include <bits/stdc++.h>
-#include <iostream>
-#include <vector>
-
-#ifdef UHATEME // Defined in Xcode Artichtecture for local development.
-
-enum InputType { STDOUT, FILE_IO, FILE_INPUT };
-
-#define CURRENT_INPUT_TYPE FILE_INPUT
-#include "uhateme.h"
-#endif
-
-using namespace std;
-
-// Template used by Ritesh Singla (alias=uahteme) for competitive programming.
-// TEMPLATE START
-namespace {
-
-// Constants
-#define endl '\n'
-
-// Loops
-#define rep(i, j, n) for (int i = (j); i < (n); i++)
-#define repv(i, j, n) for (int i = (n)-1; i >= (j); i--)
-#define set2(ar, i, j, v)                                                      \
-    rep(a, 0, i) {                                                             \
-        rep(b, 0, j) { ar[a][b] = v; }                                         \
-    }
-#define set1(ar, i, v)                                                         \
-    rep(a, 0, i) { ar[a] = v; }
-
-// Data Types
-#define int64 long long int
-#define ll long long int
-#define vi vector<int>
-
-// Short aliases
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-
-// Speeds up cin/cout. Do not use in cin/cout and scanf/printf
-// when using UseFastIO
-inline void UseFastIO(bool use_fast_io) {
-    if (use_fast_io) {
-        cin.tie(0);
-        cout.tie(0);
-        cin.sync_with_stdio(0);
-        cout.sync_with_stdio(0);
-    }
-}
-
-#ifdef UHATEME
-inline void SetInputType(InputType input_type) {
-    if (input_type == STDOUT) {
-        return;
-    }
-    if (input_type == FILE_IO) {
-        freopen(GetInputFileName(), "r", stdin);
-        freopen(GetOutputFileName(), "w", stdout);
-        return;
-    }
-    if (input_type == FILE_INPUT) {
-        freopen(GetInputFileName(), "r", stdin);
-        return;
-    }
-    cerr << "Unable to setup the input type. Please handle the error. Found "
-            "input type = "
-         << input_type;
-    abort();
-}
-#endif
-
-} // namespace
-// TEMPLATE END
-
 // This data structure maintains the minimum cost that can be achieved with a
 // given budget. It stores a map with Key-Value pairs (i,j) s.t.
 //      if we have atleast budget |i|, the associated cost is |j|.
@@ -187,52 +110,32 @@ class BudgetCostMap {
 void SolveTestCase() {
     budget_map.Clear();
     budget_map.Insert(1000, 10000);
+#ifdef UHATEME
     budget_map.DebugString();
+#endif
     budget_map.Insert(1010, 5000);
+#ifdef UHATEME
     budget_map.DebugString();
+#endif
     budget_map.Insert(1020, 100);
+#ifdef UHATEME
     budget_map.DebugString();
+#endif
     budget_map.Insert(1000, 10000000000);
+#ifdef UHATEME
     budget_map.DebugString();
+#endif
     budget_map.Insert(1000, 10000000000);
+#ifdef UHATEME
     budget_map.DebugString();
-    cout << "999 == " << budget_map.Query(999) << endl;
-    cout << "1000 == " << budget_map.Query(1000) << endl;
-    cout << "1001 == " << budget_map.Query(1001) << endl;
-    cout << "1009 == " << budget_map.Query(1009) << endl;
-    cout << "1010 == " << budget_map.Query(1010) << endl;
-    cout << "1011 == " << budget_map.Query(1011) << endl;
-    cout << "1019 == " << budget_map.Query(1019) << endl;
-    cout << "1020 == " << budget_map.Query(1020) << endl;
-    cout << "1021 == " << budget_map.Query(1021) << endl;
-}
-
-void Solve() {
-    int t;
-    cin >> t;
-    for (int i = 1; i <= t; i++) {
-#ifdef UHATEME
-        if (CURRENT_INPUT_TYPE == FILE_IO) {
-            cerr << "Starting Test Case #" << i << endl;
-        }
 #endif
-        cout << "Case #" << i << ": ";
-        SolveTestCase();
-#ifdef UHATEME
-        if (CURRENT_INPUT_TYPE == FILE_IO) {
-            cerr << "Completed Test Case #" << i << endl;
-            cout.flush();
-            cerr.flush();
-        }
-#endif
-        cout << endl;
-    }
-}
-
-int main() {
-    UseFastIO(true);
-#ifdef UHATEME
-    SetInputType(CURRENT_INPUT_TYPE);
-#endif
-    Solve();
+    cout << "999 == " << budget_map.Query(999) << endl;   // LONG_LONG_MAX
+    cout << "1000 == " << budget_map.Query(1000) << endl; // 10000
+    cout << "1001 == " << budget_map.Query(1001) << endl; // 10000
+    cout << "1009 == " << budget_map.Query(1009) << endl; // 10000
+    cout << "1010 == " << budget_map.Query(1010) << endl; // 5000
+    cout << "1011 == " << budget_map.Query(1011) << endl; // 5000
+    cout << "1019 == " << budget_map.Query(1019) << endl; // 5000
+    cout << "1020 == " << budget_map.Query(1020) << endl; // 100
+    cout << "1021 == " << budget_map.Query(1021) << endl; // 100
 }
--- templates/segment_tree.cc
+++ templates/segment_tree.cc
@@ -22,83 +22,6 @@
 //  IN THE SOFTWARE.
 //
 
-#include <algorithm>
-#include <bits/stdc++.h>
-#include <iostream>
-#include <vector>
-
-#ifdef UHATEME // Defined in Xcode Artichtecture for local development.
-
-enum InputType { STDOUT, FILE_IO, FILE_INPUT };
-
-#define CURRENT_INPUT_TYPE STDOUT
-#include "uhateme.h"
-#endif
-
-using namespace std;
-
-// Template used by Ritesh Singla (alias=uahteme) for competitive programming.
-// TEMPLATE START
-namespace {
-
-// Constants
-#define endl '\n'
-
-// Loops
-#define rep(i, j, n) for (int i = (j); i < (n); i++)
-#define repv(i, j, n) for (int i = (n)-1; i >= (j); i--)
-#define set2(ar, i, j, v)                                                      \
-    rep(a, 0, i) {                                                             \
-        rep(b, 0, j) { ar[a][b] = v; }                                         \
-    }
-#define set1(ar, i, v)                                                         \
-    rep(a, 0, i) { ar[a] = v; }
-
-// Data Types
-#define int64 long long int
-#define ll long long int
-#define vi vector<int>
-
-// Short aliases
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-
-// Speeds up cin/cout. Do not use in cin/cout and scanf/printf
-// when using UseFastIO
-inline void UseFastIO(bool use_fast_io) {
-    if (use_fast_io) {
-        cin.tie(0);
-        cout.tie(0);
-        cin.sync_with_stdio(0);
-        cout.sync_with_stdio(0);
-    }
-}
-
-#ifdef UHATEME
-inline void SetInputType(InputType input_type) {
-    if (input_type == STDOUT) {
-        return;
-    }
-    if (input_type == FILE_IO) {
-        freopen(GetInputFileName(), "r", stdin);
-        freopen(GetOutputFileName(), "w", stdout);
-        return;
-    }
-    if (input_type == FILE_INPUT) {
-        freopen(GetInputFileName(), "r", stdin);
-        return;
-    }
-    cerr << "Unable to setup the input type. Please handle the error. Found "
-            "input type = "
-         << input_type;
-    abort();
-}
-#endif
-
-} // namespace
-// TEMPLATE END
-
 class SegmentTree {
   public:
 #define NodeValue int64
@@ -174,34 +97,4 @@ void SolveTestCase() {
     rep(i, 0, 100) { tree.Update(i, i, i); }
     rep(i, 0, 100) { cout << tree.Query(i, i) << endl; }
     cout << tree.Query(28, 75) << endl;
-}
-
-void Solve() {
-    int t;
-    cin >> t;
-    for (int i = 1; i <= t; i++) {
-#ifdef UHATEME
-        if (CURRENT_INPUT_TYPE == FILE_IO) {
-            cerr << "Starting Test Case #" << i << endl;
-        }
-#endif
-        cout << "Case #" << i << ": ";
-        SolveTestCase();
-#ifdef UHATEME
-        if (CURRENT_INPUT_TYPE == FILE_IO) {
-            cerr << "Completed Test Case #" << i << endl;
-            cout.flush();
-            cerr.flush();
-        }
-#endif
-        cout << endl;
-    }
-}
-
-int main() {
-    UseFastIO(true);
-#ifdef UHATEME
-    SetInputType(CURRENT_INPUT_TYPE);
-#endif
-    Solve();
 }
