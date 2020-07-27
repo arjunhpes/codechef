a7b6bdf1ae9b705102937370cd6b7d06e849b995
Ritesh <riteshsingla@gmail.com>
2020-07-27 17:25:21 +0530

    Relying on default Google style formatting for clang

--- templates/budget_cost_map.cc
+++ templates/budget_cost_map.cc
@@ -29,78 +29,78 @@
 //      for any 2 pairs (i1,j1) and (i2,j2), i1 != i2.
 //      for any 2 pairs (i1,j1) and (i2,j2), if(i1 < i2) then j1 > j2.
 class BudgetCostMap {
-  public:
+ public:
 #define CostType int64
 #define BudgetType int
 #define INFINITY_COST LONG_LONG_MAX
-    void Clear() { budget_map_.clear(); }
+  void Clear() { budget_map_.clear(); }
 
-    // Amortized O(logn)
-    void Insert(BudgetType budget, CostType cost) {
-        auto itr = budget_map_.lower_bound(budget);
-        bool need_to_balance_by_itr = false;
+  // Amortized O(logn)
+  void Insert(BudgetType budget, CostType cost) {
+    auto itr = budget_map_.lower_bound(budget);
+    bool need_to_balance_by_itr = false;
 
-        if (itr != budget_map_.end() && itr->first == budget) {
-            // The exact key is present.
-            if (itr->second <= cost) {
-                return;
-            }
-            itr->second = cost;
-            // keep on removing excessive costs which are in front.
-            itr++;
-            need_to_balance_by_itr = true;
-        } else if (itr == budget_map_.begin()) {
-            // itr->first > budget or empty map.
-            auto itr_inserted_pair = budget_map_.insert({budget, cost});
-            itr = itr_inserted_pair.first;
-            itr++;
-            need_to_balance_by_itr = true;
-        } else if (itr == budget_map_.end()) {
-            itr--;
-            if (itr->second <= cost) {
-                return;
-            }
-            budget_map_[budget] = cost;
-        } else {
-            // itr != budget_map_.begin() && itr != budget_map_.end() &&
-            // itr->first != budget
-            itr--;
-            if (itr->second <= cost) {
-                return;
-            }
-            auto itr_inserted_pair = budget_map_.insert({budget, cost});
-            itr = itr_inserted_pair.first;
-            itr++;
-            need_to_balance_by_itr = true;
-        }
-        if (need_to_balance_by_itr) {
-            while (itr != budget_map_.end() && itr->second >= cost) {
-                itr = budget_map_.erase(itr);
-            }
-        }
+    if (itr != budget_map_.end() && itr->first == budget) {
+      // The exact key is present.
+      if (itr->second <= cost) {
+        return;
+      }
+      itr->second = cost;
+      // keep on removing excessive costs which are in front.
+      itr++;
+      need_to_balance_by_itr = true;
+    } else if (itr == budget_map_.begin()) {
+      // itr->first > budget or empty map.
+      auto itr_inserted_pair = budget_map_.insert({budget, cost});
+      itr = itr_inserted_pair.first;
+      itr++;
+      need_to_balance_by_itr = true;
+    } else if (itr == budget_map_.end()) {
+      itr--;
+      if (itr->second <= cost) {
+        return;
+      }
+      budget_map_[budget] = cost;
+    } else {
+      // itr != budget_map_.begin() && itr != budget_map_.end() &&
+      // itr->first != budget
+      itr--;
+      if (itr->second <= cost) {
+        return;
+      }
+      auto itr_inserted_pair = budget_map_.insert({budget, cost});
+      itr = itr_inserted_pair.first;
+      itr++;
+      need_to_balance_by_itr = true;
     }
-
-    // O(logn)
-    CostType Query(BudgetType budget) {
-        auto itr = budget_map_.upper_bound(budget);
-        if (itr == budget_map_.begin()) {
-            return INFINITY_COST;
-        }
-        itr--;
-        return itr->second;
+    if (need_to_balance_by_itr) {
+      while (itr != budget_map_.end() && itr->second >= cost) {
+        itr = budget_map_.erase(itr);
+      }
     }
+  }
+
+  // O(logn)
+  CostType Query(BudgetType budget) {
+    auto itr = budget_map_.upper_bound(budget);
+    if (itr == budget_map_.begin()) {
+      return INFINITY_COST;
+    }
+    itr--;
+    return itr->second;
+  }
 
 #ifdef UHATEME
-    void DebugString() {
-        for (const auto &pair : budget_map_) {
-            cout << pair.first << " -> " << pair.second << endl;
-        }
-        cout << endl;
+  void DebugString() {
+    for (const auto &pair : budget_map_) {
+      cout << pair.first << " -> " << pair.second << endl;
     }
+    cout << endl;
+  }
 #endif
 
-  private:
-    map<BudgetType, CostType> budget_map_;
+ private:
+  map<BudgetType, CostType> budget_map_;
 
 #undef CostType
 #undef BudgetType
@@ -108,34 +108,34 @@ class BudgetCostMap {
 } budget_map;
 
 void SolveTestCase() {
-    budget_map.Clear();
-    budget_map.Insert(1000, 10000);
+  budget_map.Clear();
+  budget_map.Insert(1000, 10000);
 #ifdef UHATEME
-    budget_map.DebugString();
+  budget_map.DebugString();
 #endif
-    budget_map.Insert(1010, 5000);
+  budget_map.Insert(1010, 5000);
 #ifdef UHATEME
-    budget_map.DebugString();
+  budget_map.DebugString();
 #endif
-    budget_map.Insert(1020, 100);
+  budget_map.Insert(1020, 100);
 #ifdef UHATEME
-    budget_map.DebugString();
+  budget_map.DebugString();
 #endif
-    budget_map.Insert(1000, 10000000000);
+  budget_map.Insert(1000, 10000000000);
 #ifdef UHATEME
-    budget_map.DebugString();
+  budget_map.DebugString();
 #endif
-    budget_map.Insert(1000, 10000000000);
+  budget_map.Insert(1000, 10000000000);
 #ifdef UHATEME
-    budget_map.DebugString();
+  budget_map.DebugString();
 #endif
-    cout << "999 == " << budget_map.Query(999) << endl;   // LONG_LONG_MAX
-    cout << "1000 == " << budget_map.Query(1000) << endl; // 10000
-    cout << "1001 == " << budget_map.Query(1001) << endl; // 10000
-    cout << "1009 == " << budget_map.Query(1009) << endl; // 10000
-    cout << "1010 == " << budget_map.Query(1010) << endl; // 5000
-    cout << "1011 == " << budget_map.Query(1011) << endl; // 5000
-    cout << "1019 == " << budget_map.Query(1019) << endl; // 5000
-    cout << "1020 == " << budget_map.Query(1020) << endl; // 100
-    cout << "1021 == " << budget_map.Query(1021) << endl; // 100
+  cout << "999 == " << budget_map.Query(999) << endl;    // LONG_LONG_MAX
+  cout << "1000 == " << budget_map.Query(1000) << endl;  // 10000
+  cout << "1001 == " << budget_map.Query(1001) << endl;  // 10000
+  cout << "1009 == " << budget_map.Query(1009) << endl;  // 10000
+  cout << "1010 == " << budget_map.Query(1010) << endl;  // 5000
+  cout << "1011 == " << budget_map.Query(1011) << endl;  // 5000
+  cout << "1019 == " << budget_map.Query(1019) << endl;  // 5000
+  cout << "1020 == " << budget_map.Query(1020) << endl;  // 100
+  cout << "1021 == " << budget_map.Query(1021) << endl;  // 100
 }
--- templates/segment_tree.cc
+++ templates/segment_tree.cc
@@ -23,77 +23,76 @@
 //
 
 class SegmentTree {
-  public:
+ public:
 #define NodeValue int64
-#define SEG_TREE_COMPUTATION                                                   \
-    int c1 = (cur << 1) + 1;                                                   \
-    int c2 = (cur << 1) + 2;                                                   \
-    int m = (s + e) >> 1;
+#define SEG_TREE_COMPUTATION \
+  int c1 = (cur << 1) + 1;   \
+  int c2 = (cur << 1) + 2;   \
+  int m = (s + e) >> 1;
 
-    void Make(int _n) {
-        n = _n;
-        Make(0, 0, n - 1);
+  void Make(int _n) {
+    n = _n;
+    Make(0, 0, n - 1);
+  }
+
+  void Update(int S, int E, NodeValue val) { Update(0, 0, n - 1, S, E, val); }
+
+  NodeValue Query(int S, int E) { return Query(0, 0, n - 1, S, E); }
+
+ private:
+  void Make(int cur, int s, int e) {
+    if (s == e) {
+      tree[cur] = LONG_LONG_MAX;
+      return;
+    }
+    SEG_TREE_COMPUTATION
+    Make(c1, s, m);
+    Make(c2, m + 1, e);
+    tree[cur] = LONG_LONG_MAX;
+  }
+
+  // [s, e] -> tree range. [S, E] -> range of interest.
+  void Update(int cur, int s, int e, int S, int E, NodeValue val) {
+    // node not of interest.
+    if (s > E || e < S) return;
+    // node completely inside interest range. Mark for laze update if
+    // needed.
+    if (s >= S && e <= E) {
+      // This is just a palceholder. In reality, add lazy propagation for
+      // min segtree.
+      tree[cur] = val;
+      return;
+    }
+    SEG_TREE_COMPUTATION
+    Update(c1, s, m, S, E, val);
+    Update(c2, m + 1, e, S, E, val);
+    tree[cur] = min(tree[c1], tree[c2]);
+  }
+
+  NodeValue Query(int cur, int s, int e, int S, int E) {
+    if (s > E || e < S) {
+      return LONG_LONG_MAX;
     }
 
-    void Update(int S, int E, NodeValue val) { Update(0, 0, n - 1, S, E, val); }
-
-    NodeValue Query(int S, int E) { return Query(0, 0, n - 1, S, E); }
-
-  private:
-    void Make(int cur, int s, int e) {
-        if (s == e) {
-            tree[cur] = LONG_LONG_MAX;
-            return;
-        }
-        SEG_TREE_COMPUTATION
-        Make(c1, s, m);
-        Make(c2, m + 1, e);
-        tree[cur] = LONG_LONG_MAX;
+    if (s >= S && e <= E) {
+      return tree[cur];
     }
+    SEG_TREE_COMPUTATION
+    NodeValue a = Query(c1, s, m, S, E);
+    NodeValue b = Query(c2, m + 1, e, S, E);
+    return min(a, b);
+  }
 
-    // [s, e] -> tree range. [S, E] -> range of interest.
-    void Update(int cur, int s, int e, int S, int E, NodeValue val) {
-        // node not of interest.
-        if (s > E || e < S)
-            return;
-        // node completely inside interest range. Mark for laze update if
-        // needed.
-        if (s >= S && e <= E) {
-            // This is just a palceholder. In reality, add lazy propagation for
-            // min segtree.
-            tree[cur] = val;
-            return;
-        }
-        SEG_TREE_COMPUTATION
-        Update(c1, s, m, S, E, val);
-        Update(c2, m + 1, e, S, E, val);
-        tree[cur] = min(tree[c1], tree[c2]);
-    }
-
-    NodeValue Query(int cur, int s, int e, int S, int E) {
-        if (s > E || e < S) {
-            return LONG_LONG_MAX;
-        }
-
-        if (s >= S && e <= E) {
-            return tree[cur];
-        }
-        SEG_TREE_COMPUTATION
-        NodeValue a = Query(c1, s, m, S, E);
-        NodeValue b = Query(c2, m + 1, e, S, E);
-        return min(a, b);
-    }
-
-    NodeValue tree[6 * 10000005];
-    int n;
+  NodeValue tree[6 * 10000005];
+  int n;
 
 #undef SEG_TREE_COMPUTATION
 #undef NodeValue
 } tree;
 
 void SolveTestCase() {
-    tree.Make(100);
-    rep(i, 0, 100) { tree.Update(i, i, i); }
-    rep(i, 0, 100) { cout << tree.Query(i, i) << endl; }
-    cout << tree.Query(28, 75) << endl;
+  tree.Make(100);
+  rep(i, 0, 100) { tree.Update(i, i, i); }
+  rep(i, 0, 100) { cout << tree.Query(i, i) << endl; }
+  cout << tree.Query(28, 75) << endl;
 }
